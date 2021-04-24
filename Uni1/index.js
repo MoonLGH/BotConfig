bdf4dccd1ffaea7c8db216851539805b39cd7e54
MoonLGH <MoonLMC1212@gmail.com>
2021-04-24 22:58:56 +0700

    KWOAKOWAO

--- Uni1/index.js
+++ Uni1/index.js
@@ -11,29 +11,13 @@ if (hour >= 18) {
     welcomer.innerHTML = "Good Night"
 }
 
-function pw(checked) {
-    const passel = document.getElementById("McPass")
-    if (checked == false) {
-        passel.setAttribute("disabled", checked)
-        passel.placeholder = "CRACKED"
-    }
-    if (checked == true) {
-        passel.removeAttribute("disabled")
-        passel.value = ""
-        passel.placeholder = "Your Minecraft Account Password"
-    }
-}
+
 function genout(){
     document.getElementById("OUT").innerHTML = `
     {
         "Dtoken": "${valueof("DToken")}",
         "prefix": "${valueof("Dprefix")}",
-        "username": "${valueof("McEmail")}",
-        "password":"${valueof("McPass")}",
-        "ip":"${valueof("McServerIP")}",
-        "version": "${document.getElementById("McServerVer").options[document.getElementById("McServerVer").selectedIndex].value}",
-        "scid": "${valueof("CHIDSC")}",
-        "jc" : "${valueof("McJC")}"
+        "scid": "${valueof("CHIDSC")}"
       }`
     console.log("one of them changed")
 }
